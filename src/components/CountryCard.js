382a8583cec2093b58e11dc10ed597a0d1396776
Mohirjon013 <mohirjon26@gmail.com>
2024-09-18 02:36:26 +0100

    start project

---
+++ src/components/CountryCard.js
@@ -0,0 +1,17 @@
+
+
+function CountryCard({item}){
+    return(
+        <li className="w-[264px] rounded-md bg-white pb-[46px]">
+            <img className="rounded-t-md object-cover h-[160px] pb-[24px]" src={item.flag} alt="Flag" width={"100%"}/>
+        
+            <div className="pl-[24px]">
+                <strong className="font-[700] text-[18px] leading-[26px] ">{item.name}</strong>
+                <p className="font-[300] text-[14px] leading-[16px] mt-[16px]"> <span className="font-[600] text-[14px] leading-[16px]">Population: </span> {item.population}</p>
+                <p className="font-[300] text-[14px] leading-[16px] my-[8px]"><span className="font-[600] text-[14px] leading-[16px]">Region: </span> {item.name}</p>
+                <p className="font-[300] text-[14px] leading-[16px]"><span className="font-[600] text-[14px] leading-[16px]">Capital: </span> {item.capital}</p>
+            </div>
+        </li>   
+    )
+}
+export default CountryCard
